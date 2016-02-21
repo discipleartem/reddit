b433501e96c031b92f8bc95175bd09233a29977a
Tomas <magyc@rambler.ru>
2016-02-21 16:33:28 +0200

    Added and setup acts_as_votable

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'links#index'
-  resources :links
+
+  devise_for :users
+
+  resources :links do
+    member do
+      put "like",    to: 'links#upvote'
+      put "dislike", to: 'links#downvote'
+    end
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
